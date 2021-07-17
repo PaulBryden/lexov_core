f8f506951d7e1bcb9d4ad9c718ac2201da77480e
Paul Bryden <p.d.bry@outlook.com>
2021-07-16 20:17:44 -0700

    Akllow clone of network structs

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ pub mod data_structures {
     use serde::{Deserialize, Serialize};
     use ts_rs::{export, TS};
 
-    #[derive(Serialize, Deserialize, TS)]
+    #[derive(Serialize, Deserialize, TS, Clone)]
     pub struct Position {
         pub x: f64,
         pub y: f64,
