5a6edad1489b1a4201663f17a37e92cb42ae5e0e
Paul Bryden <p.d.bry@outlook.com>
2021-07-16 23:26:41 -0700

    Testing dependencies

---
+++ dist/structures.d.ts
@@ -0,0 +1,12 @@
+export interface Position {
+    x: number;
+    y: number;
+    z: number;
+}
+export interface Player {
+    uuid: number;
+    position: Position;
+}
+export interface PlayerList {
+    players: Player[];
+}
