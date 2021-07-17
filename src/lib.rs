724313b59803cf7713339a998041afc6b9da63ac
Paul Bryden <p.d.bry@outlook.com>
2021-07-16 23:22:18 -0700

    Added copy derivation

--- src/lib.rs
+++ src/lib.rs
@@ -3,21 +3,21 @@ pub mod data_structures {
     use serde::{Deserialize, Serialize};
     use ts_rs::{export, TS};
 
-    #[derive(Serialize, Deserialize, TS, Clone)]
+    #[derive(Serialize, Deserialize, TS, Clone, Copy)]
     pub struct Position {
         pub x: f64,
         pub y: f64,
         pub z: f64,
     }
 
-    #[derive(Serialize, Deserialize, TS, Clone)]
+    #[derive(Serialize, Deserialize, TS, Clone, Copy)]
     pub struct Player {
         pub uuid: u64,
         pub position: Position,
     }
     
     
-    #[derive(Serialize, Deserialize, TS, Clone)]
+    #[derive(Serialize, Deserialize, TS, Clone )]
     pub struct PlayerList {
         pub players: Vec<Player>,
     }
